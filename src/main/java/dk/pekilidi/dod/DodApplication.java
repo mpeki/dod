bf94c39dd1d82725ca7ae9f63aedf7502b7bb5f6
msp <msp@tiatechnology.com>
2021-11-02 11:40:10 +0100

    initial work

---
+++ src/main/java/dk/pekilidi/dod/DodApplication.java
@@ -0,0 +1,12 @@
+package dk.pekilidi.dod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DodApplication.class, args);
+  }
+}
---
+++ src/test/java/dk/pekilidi/dod/DodApplicationTests.java
@@ -0,0 +1,12 @@
+package dk.pekilidi.dod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
