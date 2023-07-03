eaa6c27adbfd3e93657750e60d25df02cab8821a
mpeki <mpekilidi@gmail.com>
2023-07-03 14:59:50 +0200

    fix(DOD-249): Improve code stability

--- backend/src/main/java/dk/pekilidi/dod/validation/RegularExpressions.java
+++ backend/src/main/java/dk/pekilidi/dod/validation/RegularExpressions.java
@@ -2,4 +2,8 @@ package dk.pekilidi.dod.validation;
 
 public class RegularExpressions {
   public static final String VALID_CHARACTER_NAME = "^(?:Mr\\. |Mrs\\. )?(?:[A-Za-z]+[- ]?)*[A-Za-z]+(?: Jr\\.)?$";
+
+  private RegularExpressions() {
+    throw new IllegalStateException("Utility class");
+  }
 }
